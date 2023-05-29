d257d149eaf9d47fe75b2d42a9d476723457cddd
Trino <trinogamer11@gmail.com>
2023-05-29 17:49:27 +0200

    API prod URL changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    api_url:"https://192.168.0.41:3000/v1",
-    api_login_url:"https://192.168.0.41:3001/v1",
+    api_url:"https://api.trinohost.com/v1",
+    api_login_url:"https://auth.trinohost.com:3001/v1",
   };
