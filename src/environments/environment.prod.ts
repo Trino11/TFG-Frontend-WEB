435576728a9347cf470975772297ba65b0d03af3
Antonio Manuel Rebollar Marquez <rebollar.marquez.antoniomanuel@gmail.com>
2023-05-29 17:46:12 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     api_url:"https://api.trinohost.com/v1",
-    api_login_url:"https://auth.trinohost.com:3001/v1",
+    api_login_url:"https://auth.trinohost.com/v1",
   };
